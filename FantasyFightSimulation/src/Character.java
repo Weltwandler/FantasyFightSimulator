0db2372839acab52f44d22c67a35476d039fa81a
Weltwandler <manuelgebhard@gmail.com>
2019-05-11 19:56:33 +1200

    Initial Commit

---
+++ FantasyFightSimulation/src/Character.java
@@ -0,0 +1,24 @@
+
+public class Character {
+	private int hitPoints;
+	private int armor;
+	private int attack;
+	private int damage;
+	
+	public int getHitPoints() {
+		return hitPoints;
+	}
+	
+	public void setHitPoints(int num) {
+		hitPoints = num;
+	}
+	
+	public int getArmor() {
+		return armor;
+	}
+	
+	public void setArmor(int num) {
+		armor = num;
+	}
+	
+}
